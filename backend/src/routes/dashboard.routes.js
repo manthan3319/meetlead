03064cc8655efc7e01d503bfaf7ec8b4b9b97871
manthan vaghasiya <vaghasiyamanthan5@gmail.com>
2026-05-20 14:42:23 +0530

    web: isolated branch (only web/ contents)

--- backend/src/routes/dashboard.routes.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const c = require('../controllers/dashboard.controller');
-const { protect } = require('../middleware/auth');
-
-router.use(protect);
-router.get('/summary', c.summary);
-router.get('/pipeline', c.pipeline);
-
-module.exports = router;
--- mobile/lib/config/constants.dart
+++
@@ -1,6 +0,0 @@
-class AppConstants {
-  static const String apiBaseUrl = 'http://localhost:5050/api';
-  static const String socketUrl = 'http://localhost:5050';
-  static const String tokenKey = 'auth_token';
-  static const String userKey = 'auth_user';
-}
